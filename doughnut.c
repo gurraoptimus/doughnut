149e6cc3a5e9982abfbd918db0f75f19937e1833
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-08-19 14:19:52 +0200

    doughnut

--- doughnut.c
+++ doughnut.c
@@ -1,7 +1,7 @@
        k;double sin()
      ,cos();main(){float A=
     0,B=0,i,j,z[1760];char b[
-  1760];printf("\x1b[2J");for(;;
+   1760];printf("\x1b[2J");for(;;
  ){memset(b,32,1760);memset(z,0,7040)
  ;for(j=0;6.28>j;j+=0.07)for(i=0;6.28
  >i;i+=0.02){float c=sin(i),d=cos(j),e=
@@ -12,5 +12,5 @@ in(B),t=c*h*g-f*        e;int x=40+30*D*
 +t*m),o=x+80*y,          N=8*((f*e-c*d*g
  )*m-c*d*e-f*g-l        *d*n);if(22>y&&
  y>0&&x>0&&80>x&&D>z[o]){z[o]=D;;;b[o]=
-  ".,-~:;!*#$@"[N>0?N:0];}}/*#****!!-*/
-   printf()
+ ".,-~:;!*#$@"[N>0?N:0];}}/*#****!!-*/
+   printf("\x1b[H")
