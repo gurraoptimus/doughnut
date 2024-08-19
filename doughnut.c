30a7bef0bbadef3cf4134b63b56fb4a7a5d651b2
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-08-19 13:50:11 +0200

    doughnut

--- doughnut.c
+++ doughnut.c
@@ -5,4 +5,7 @@
 ){memset(b,32,1760);memset(z,0,7040)
 ;for(j=0;6.28>j;j+=0.07)for(i=0;6.28
 >i;i+=0.02){float c=sin(i),d=cos(j),e=
-sin(A)
+sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*
+h*e+f*g+5),l=cos      (i),m=cos(B),n=s\
+in(B),t=c*h*g-f*       e;int x=40+30*D*
+(l*h*m-t*n),y=            12+15
